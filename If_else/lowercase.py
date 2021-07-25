688f7e11cfc49fa53bf8d2fda7fb959006c39759
Divya Soni <divyasoni011@gmail.com>
2021-07-25 23:37:19 +0530

    Add files via upload

---
+++ If_else/lowercase.py
@@ -0,0 +1,35 @@
+# x = input("Enter: ")
+# new = " "
+# for i in x:
+#     y = ord(i)
+#     if y >= 97 and y <= 122:
+#         new += i
+#     else:
+#         y += 32
+#         y = chr(y)
+#         new += y
+# print(new)
+
+
+# Delete alternate character in a string:
+# x = "RUVYA SHIP"
+# new = " "
+# for i in range(len(x)):
+#     if i % 2 == 0:
+#         new += x[i]
+# print(new)
+
+
+
+# for i in x:
+#     y = ord(i)
+#     if y == 32:
+#         pass
+#     else:
+#         new += i
+# print(new)
+
+names = [ "Geek", "Geeks", "Geeksfor",
+  "GeeksforGeek", "GeeksforGeeks" ]
+len(names)
+  
