04a3358e6a0db6d8f14cb1a231412d9ca7aca974
Divya Soni <divyasoni011@gmail.com>
2021-08-02 17:55:33 +0530

    Add files via upload

---
+++ If_else/q15.py
@@ -0,0 +1,8 @@
+x = int(input("x = "))
+y = int(input("y = "))
+z = int(input("z = "))
+a = x + y + z
+if a == 180 and x!= 0 and y!= 0 and z!= 0:
+    print("possible")
+else:
+    print("not possible")
